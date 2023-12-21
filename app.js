14ddb56b20031efd931d616d7e21b6f18a582408
Sheikh Umair <umair.najeeb@iquasar.com>
2023-12-21 20:24:54 +0530

    GetById

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const fs = require('fs');
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -14,20 +14,30 @@ app.get('/api/v1/tours', (req, res) => {
     tours,
   });
 });
-app.post('/api/v1/tours', (req, res)=>{
-    const newId = tours.at(tours.length-1).id + 1
-    const newTour = Object.assign({id : newId}, req.body)
-    tours.push(newTour)
-    console.log(tours)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err)=>{
-        res.status(201).json({
-            status : "success",
-            data : {
-                tour : newTour
-            }
-        })
-    })
-})
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.at(tours.length - 1).id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  console.log(tours);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req);
+  res.status(200).json({
+    status: 'success',
+  });
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
