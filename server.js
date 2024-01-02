8edc74f313a91dda9dc0ab88de620f15edd8bb12
Sheikh Umair <umair.najeeb@iquasar.com>
2024-01-02 18:28:54 +0530

    Handled undefined routes and added global error handler

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require('express');
-const tourRouter = require("./routes/tourRoutes")
+const tourRouter = require('./routes/tourRoutes');
 const connectToMongoose = require('./config/connectToDb');
-connectToMongoose()
+connectToMongoose();
 const app = express();
 
-app.use(express.json())
-app.use("/api/v1/tours", tourRouter)
+app.use(express.json());
+app.use('/api/v1/tours', tourRouter);
 
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Cant find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'Fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'Error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);  
+  console.log(`Server is running on ${PORT}`);
 });
